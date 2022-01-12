27a964a388e5f0299cf7cc26c2034b303426491d
Onur Kara <onurkara92@gmail.com>
2022-01-13 00:57:29 +0300

    Add empty repository

---
+++ MoviesApp/MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Onur on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
