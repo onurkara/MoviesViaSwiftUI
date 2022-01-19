242b7b082dcacb44b396d3e547888856f5d526ef
Onur Kara <onurkara92@gmail.com>
2022-01-19 22:56:15 +0300

    Fetch latest movies async

--- MoviesApp/MoviesApp/Repository/MovieRepository.swift
+++ MoviesApp/MoviesApp/Repository/MovieRepository.swift
@@ -5,9 +5,12 @@
 //  Created by Onur on 16.01.2022.
 //
 
+import Darwin
+
 protocol MovieRepositoryProtocol {
 
     func fetchPopularMovies() async throws -> Result<[Movie], BaseError>
+    func fetchLatestMovies() async throws -> Result<[Movie], BaseError>
 }
 
 final class MovieRepository: MovieRepositoryProtocol {
@@ -16,6 +19,15 @@ final class MovieRepository: MovieRepositoryProtocol {
         do {
             let popularMoviesResponse: MovieResponse? = try await NetworkManager.shared.submitRequest(request: PopularMoviesRequest())
             return .success(popularMoviesResponse?.results ?? [])
+        } catch {
+            return .failure(.requestFailed)
+        }
+    }
+
+    func fetchLatestMovies() async throws -> Result<[Movie], BaseError> {
+        do {
+            let latestMoviesResponse: MovieResponse? = try await NetworkManager.shared.submitRequest(request: LatestMoviesRequest())
+            return .success(latestMoviesResponse?.results ?? [])
         } catch {
             return .failure(.requestFailed)
         }
